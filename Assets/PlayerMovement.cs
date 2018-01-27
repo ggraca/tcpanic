91a9b38c7cafe76b12c621c4122029580da31142
GGraca <ggracase@gmail.com>
2018-01-27 12:16:06 +0000

    Added initial prefabs

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	Rigidbody2D rb;
+	bool jumping = false;
+	public int jump_multiplier = 30;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("up") && !jumping){
+			rb.AddForce(Vector3.up * jump_multiplier);
+		}
+	}
+}
