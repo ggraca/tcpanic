1984042b150c3aa8da119918a1abaeaf3174591c
GGraca <ggracase@gmail.com>
2018-01-27 18:23:43 +0000

    cenas

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = FindPlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player == null)
+			player = FindPlayer();
+
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z) ;
+	}
+
+	GameObject FindPlayer(){
+		return (GameObject) GameObject.FindGameObjectWithTag("Player");
+	}
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+	public GameObject prefab;
+	public Texture2D level;
+	
+	// Use this for initialization
+	void Start () {
+		GenerateLevel();
+	}
+
+	void GenerateLevel(){
+		for(int x = 0; x < level.width; x++){
+			for(int y = 0; y < level.height; y++){
+				GenerateTile(x, y);
+			}
+		}
+	}
+
+	void GenerateTile(int x, int y){
+		Color color = level.GetPixel(x, y);
+
+		if(color.a != 0)
+			Instantiate(prefab, new Vector3(x, y), Quaternion.identity);
+	}
+}
