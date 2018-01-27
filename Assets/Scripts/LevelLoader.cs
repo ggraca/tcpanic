62972976dcddddf5b300c685fb8fe81c0111a62f
GGraca <ggracase@gmail.com>
2018-01-27 20:06:55 +0000

    removed filled lines

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour {
 
-	public GameObject prefab;
+	public GameObject block;
+	public GameObject blockSprite;
 	public Texture2D level;
 	
 	// Use this for initialization
@@ -21,9 +22,45 @@ public class LevelLoader : MonoBehaviour {
 	}
 
 	void GenerateTile(int x, int y){
-		Color color = level.GetPixel(x, y);
+		if(!PixelHasTerrain(x, y))
+			return;
 
-		if(color.a != 0)
-			Instantiate(prefab, new Vector3(x, y), Quaternion.identity);
+		GameObject parent = Instantiate(block, new Vector3(x, y), Quaternion.identity);
+		GameObject sprite;
+
+		if(!PixelHasTerrain(x, y + 1)){
+			sprite = Instantiate(blockSprite, new Vector3(x, y), Quaternion.identity);
+			sprite.transform.parent = parent.transform;
+		}
+
+		if(!PixelHasTerrain(x - 1, y)){
+			sprite = Instantiate(blockSprite, new Vector3(x, y), Quaternion.identity);
+			sprite.transform.Rotate(0, 0, 90);
+			sprite.transform.parent = parent.transform;
+		}
+
+		if(!PixelHasTerrain(x, y - 1)){
+			sprite = Instantiate(blockSprite, new Vector3(x, y), Quaternion.identity);
+			sprite.transform.Rotate(0, 0, 180);
+			sprite.transform.parent = parent.transform;
+		}
+
+		if(!PixelHasTerrain(x + 1, y)){
+			sprite = Instantiate(blockSprite, new Vector3(x, y), Quaternion.identity);
+			sprite.transform.Rotate(0, 0, 270);
+			sprite.transform.parent = parent.transform;
+		}
+		
 	}
+
+	bool PixelHasTerrain(int x, int y){
+		if(x < 0 || x >= level.width)
+			return false;
+		if(y < 0 || y >= level.height)
+			return false;
+
+		Color color = level.GetPixel(x, y);
+		return !(color.a == 0);
+	}
+
 }
